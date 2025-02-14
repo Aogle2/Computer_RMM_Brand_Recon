f99f43e81a5ba9d9f9f8ac8e241747547b3e72ef
Aaron Ogle <aaronogle7@gmail.com>
2025-02-14 04:15:43 -0500

    Putting down more note and make this look better code wise.

--- userinteractionv2.py
+++ userinteractionv2.py
@@ -52,8 +52,10 @@ class MainWindow(tkinter.Tk):
         self.newPlot(title="Test Graph", xlabel="Catagory?", ylabel="Value", parent=self.f2, xdata=pd.Series(list(stuff2.keys())),ydata=pd.Series(list(stuff2.values())))
 
 #       Setting up frame3 or "f3"
+
+        #this can be improved with a better view.
         stuff = self.baseQuery("SELECT * FROM manufacturer_view ORDER BY count DESC LIMIT 4;")
-        self.newPlot(title="Test Graph", xlabel=f"Manufacturer", ylabel="Value",parent=self.f3,xdata=stuff['Vendor Name'].astype(str),ydata=stuff['Count'])
+        self.newPlot(title="Test Graph", xlabel=f"Manufacture", ylabel="Value",parent=self.f3,xdata=stuff['Vendor Name'].astype(str),ydata=stuff['Count'])
 
 #       Setting up frame4 or "f4"
 
