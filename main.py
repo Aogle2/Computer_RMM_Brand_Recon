f0a4b66a29c8b2c8703ac3c462a7acc95d733bc5
Aaron Ogle <aaronogle7@gmail.com>
2025-05-30 06:57:24 -0400

    Ugh.

--- main.py
+++ main.py
@@ -4,13 +4,30 @@ This revision is going to add
 1. Lazy loading for each tab.
 2. Correct font loading and font size for each platform.
 3. themes
-4. External DB
+4. External DB May use this module: https://docs.devart.com/python/mysql/connect-python-apps-using-ssh.htm
 
 Modules Needed overall (WIP)
 1. Matplotlib
+    A. Display our charts.
+
 2. Tkinter
-3. mysql.connector
+    A. Display stuff
+
+3. devart-mysql-connector
+    A. Connect to the MySQL server remotely using SSH
+
 4. OS
+    A. Gather Basic Info regarding the computer running this.
 5. sys
+    A. Gather more basic info regarding the computer running this.
+6. Platform
+    A. Gather Even more basic data about the computer.
+
+
+Features I want to add
+1. Welcome screen, introducing me or something.
+    A. Buttons to show Info about computer stuff and program info
+2. Charts and Data screen
+    A. Selectable Charts with Selectable Data via drop down selections.
 
 '''
