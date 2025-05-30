1c99ed9c5971511143589eb826b54310e9d9bfc3
Aaron Ogle <aaronogle7@gmail.com>
2025-05-30 06:40:09 -0400

    Laying out the plan on main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+This revision is going to add
+
+1. Lazy loading for each tab.
+2. Correct font loading and font size for each platform.
+3. themes
+4. External DB
+
+Modules Needed overall (WIP)
+1. Matplotlib
+2. Tkinter
+3. mysql.connector
+4. OS
+5. sys
+
+'''
